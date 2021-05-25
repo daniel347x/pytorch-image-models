8bad97d212883b505750107d96ccd2fbf596431c
danissen <danissen@microsoft.com>
2021-05-24 22:52:15 -0400

    Prepare for fully CSP-integrated training run of segmentation model (swapped backbone)

--- timm/models/cspnet.py
+++ timm/models/cspnet.py
@@ -478,14 +478,14 @@ class CspNetTiny(nn.Module):
                     m.zero_init_last_bn()
 
     def forward_features(self, x):
-        print(f'***\nSTAGE 0 INPUT: x.shape {x.shape}\n***')
+        # print(f'***\nSTAGE 0 INPUT: x.shape {x.shape}\n***')
         x = self.stem(x)
-        print(f'***\nSTAGE 0 OUTPUT: x.shape {x.shape}\n***')
+        # print(f'***\nSTAGE 0 OUTPUT: x.shape {x.shape}\n***')
         features = [x]
         for i in range(3):
-            print(f'***\nSTAGE {i+1} INPUT: x.shape {x.shape}\n***')
+            # print(f'***\nSTAGE {i+1} INPUT: x.shape {x.shape}\n***')
             x = self.stages[i](x)
-            print(f'***\nSTAGE {i+1} OUTPUT: x.shape {x.shape}\n***')
+            # print(f'***\nSTAGE {i+1} OUTPUT: x.shape {x.shape}\n***')
             features.append(x)
         return features
 
